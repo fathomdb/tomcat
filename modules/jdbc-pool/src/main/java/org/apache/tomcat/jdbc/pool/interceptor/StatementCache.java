35cd896ba9ba25d186944dbc90b464469981cc39
Justin Santa Barbara <justin@fathomdb.com>
2013-02-23 19:38:12 -0800

    Make some private methods private

--- modules/jdbc-pool/src/main/java/org/apache/tomcat/jdbc/pool/interceptor/StatementCache.java
+++ modules/jdbc-pool/src/main/java/org/apache/tomcat/jdbc/pool/interceptor/StatementCache.java
@@ -172,11 +172,11 @@ public class StatementCache extends StatementDecoratorInterceptor {
     }
 
 
-    public CachedStatement removeFromCache(String sql) {
+    private CachedStatement removeFromCache(String sql) {
         return statements.remove(sql);
     }
 
-    public boolean cacheStatement(CachedStatement proxy) {
+    private boolean cacheStatement(CachedStatement proxy) {
         if (proxy.getSql()==null) {
             return false;
         } else if (statements.containsKey(proxy.getSql())) {
@@ -188,7 +188,7 @@ public class StatementCache extends StatementDecoratorInterceptor {
         }
     }
 
-    public boolean removeStatement(CachedStatement proxy) {
+    private boolean removeStatement(CachedStatement proxy) {
         if (statements.remove(proxy.getSql()) != null) {
             return true;
         } else {
